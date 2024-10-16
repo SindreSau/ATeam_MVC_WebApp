5913f39cfc0b83ca703a37e8e9012d4b24854ae2
SindreSau <sindre_50@hotmail.com>
2024-10-16 21:21:35 +0200

    init commit

---
+++ ATeam_MVC_WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ATeam_MVC_WebApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
---
+++ ATeam_MVC_WebApp/README.md
@@ -0,0 +1,2 @@
+# ATeam_MVC_WebApp
+Exam assignment for ITPE3200 Web Applications, 2024H - OsloMet.
