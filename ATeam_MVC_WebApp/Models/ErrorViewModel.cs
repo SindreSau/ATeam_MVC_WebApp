e73271470f0734ecc38cd803a14dea19aec35323
SindreSau <sindre_50@hotmail.com>
2024-10-16 22:01:18 +0200

    feat: scaffolded example folder structure

--- ATeam_MVC_WebApp/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace ATeam_MVC_WebApp.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
