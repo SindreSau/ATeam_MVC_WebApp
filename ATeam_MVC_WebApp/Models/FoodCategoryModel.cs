7f22a856f57a67b31a393742ce56565e4282bf85
SindreSau <sindre_50@hotmail.com>
2024-10-18 10:55:29 +0200

    hotfix: removed unnecessary comments and initialized nullable strings

--- ATeam_MVC_WebApp/Models/FoodCategoryModel.cs
+++ ATeam_MVC_WebApp/Models/FoodCategoryModel.cs
@@ -2,12 +2,12 @@ using System.ComponentModel.DataAnnotations;
 
 namespace ATeam_MVC_WebApp.Models
 {
-  class FoodCategory
+  public class FoodCategory
   {
-    public int CategoryID { get; set; } // Unique ID for category
+    public int CategoryId { get; set; }
 
     [Required]
-    public string CategoryName { get; set; } // Can't be null
+    public string CategoryName { get; set; } = string.Empty;
 
     // public string Description { get; set; } = string.Empty; // In case we want to add brief descriptions of the Categories (i.e. Example products that are in each category)
   }
