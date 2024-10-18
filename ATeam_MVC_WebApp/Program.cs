edfc263df61f691e061266d834a501bf788e1257
SindreSau <sindre_50@hotmail.com>
2024-10-18 13:09:37 +0200

    feat: added serilog with rotating file handler and structured logging

--- ATeam_MVC_WebApp/Program.cs
+++ ATeam_MVC_WebApp/Program.cs
@@ -1,37 +1,75 @@
 using ATeam_MVC_WebApp.Data;
+using Microsoft.AspNetCore.Hosting.Server.Features;
 using Microsoft.EntityFrameworkCore;
+using Serilog;
+using Serilog.Events;
 
-// === BUILDER CONFIGURATION === //
-var builder = WebApplication.CreateBuilder(args);
+// === SERILOG CONFIGURATION === //
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Debug()
+    .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+    .Enrich.FromLogContext()
+    .WriteTo.Console()
+    .WriteTo.File("logs/log-.log", rollingInterval: RollingInterval.Day)
+    .WriteTo.Seq("http://localhost:5341")
+    .CreateBootstrapLogger();
 
-// Add controllers with support for views
-builder.Services.AddControllersWithViews();
-
-// Configure the database
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
-
-builder.Services.AddRazorPages(); // Add Razor Pages as these are used for Identity
-
-
-// === APP CONFIGURATION === //
-var app = builder.Build();
-
-if (!app.Environment.IsDevelopment())
+try
 {
-    app.UseHsts();
+    Log.Information("Starting web application");
+    Log.Information("Creating builder...");
+
+    // === BUILDER CONFIGURATION === //
+    var builder = WebApplication.CreateBuilder(args);
+
+    // Add Serilog
+    builder.Host.UseSerilog((context, services, configuration) => configuration
+        .ReadFrom.Configuration(context.Configuration)
+        .ReadFrom.Services(services)
+        .Enrich.FromLogContext()
+        .WriteTo.Console()
+        .WriteTo.File("logs/log-.log", rollingInterval: RollingInterval.Day)
+        .WriteTo.Seq("http://localhost:5341"));
+
+    // Add controllers with support for views
+    builder.Services.AddControllersWithViews();
+
+    // Configure the database
+    builder.Services.AddDbContext<ApplicationDbContext>(options =>
+        options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+    builder.Services.AddRazorPages(); // Add Razor Pages as these are used for Identity
+
+    // === APP CONFIGURATION === //
+    Log.Information("Configuring application...");
+    var app = builder.Build();
+
+    if (!app.Environment.IsDevelopment())
+    {
+        app.UseHsts();
+    }
+
+    app.UseHttpsRedirection();
+    app.UseStaticFiles();
+
+    app.UseRouting();
+
+    app.UseAuthentication();
+    app.UseAuthorization();
+
+    app.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}");
+
+    Log.Information("Application started");
+    Log.Information("Application is running on https://localhost:7177/");
+    app.Run();
 }
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthentication();
-app.UseAuthorization();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-
-app.Run();
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Application terminated unexpectedly");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
