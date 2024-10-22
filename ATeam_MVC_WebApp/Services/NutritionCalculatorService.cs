eed1b3ed767ea75daca4aaa8bc20ecd795cee5c3
olindland <s374942@oslomet.no>
2024-10-22 18:40:37 +0200

    updated nutrition

--- ATeam_MVC_WebApp/Services/NutritionCalculatorService.cs
+++ ATeam_MVC_WebApp/Services/NutritionCalculatorService.cs
@@ -14,19 +14,7 @@ public static class NutritionCalculatorService
         const float minFiber = 6; // gram per 100g
         const float maxSalt = 1; // gram per 100g
         
-<<<<<<< HEAD
-        if (energyKcal <= max_energyKcal && fat <= max_fat && carbohydrates <= max_carbohydrates
-         && protein >= min_protein && fiber >= min_fiber && salt <= max_salt)
-        {
-            return true;
-        }
-        else
-        {
-           return false;
-        }
-=======
         return energyKcal <= maxEnergyKcal && fat <= maxFat && carbohydrates <= maxCarbohydrates
                && protein >= minProtein && fiber >= minFiber && salt <= maxSalt;
->>>>>>> origin/staging
     }
 }
