47098ca87c7798e71984b635130cd629ada5ba30
SindreSau <sindre_50@hotmail.com>
2024-10-22 17:39:33 +0200

    feat: updated program.cs to point to VendorController.cs -> Index method. Updated VendorController.cs to use new category naming

--- ATeam_MVC_WebApp/Program.cs
+++ ATeam_MVC_WebApp/Program.cs
@@ -105,7 +105,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Vendor}/{action=Index}/{id?}");
 
 app.MapRazorPages();
 
