8188394fb2381d749cc58279d3d3d12eb91fea00
SindreSau <sindre_50@hotmail.com>
2024-10-23 08:48:03 +0200

    feat: added a product that qualifies as well

--- ATeam_MVC_WebApp/Services/NutritionCalculatorService.cs
+++ ATeam_MVC_WebApp/Services/NutritionCalculatorService.cs
@@ -1,10 +1,9 @@
-
 namespace ATeam_MVC_WebApp.Services;
 
 public static class NutritionCalculatorService
 {
-    public static bool IsNokkelhullQualified(float energyKcal, float fat, float carbohydrates,
-        float protein, float fiber, float salt)
+    public static bool IsNokkelhullQualified(float energyKcal, float protein, float carbohydrates,
+        float fat, float fiber, float salt)
     {
         //Krav kan endres eller legges til her
         const float maxEnergyKcal = 40; //gram per 100g
@@ -13,7 +12,7 @@ public static class NutritionCalculatorService
         const float minProtein = 3; // gram per 100g
         const float minFiber = 6; // gram per 100g
         const float maxSalt = 1; // gram per 100g
-        
+
         return energyKcal <= maxEnergyKcal && fat <= maxFat && carbohydrates <= maxCarbohydrates
                && protein >= minProtein && fiber >= minFiber && salt <= maxSalt;
     }
